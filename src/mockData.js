11273eb42b51b3aca772cb31d6c14ca5b87a6dd3
moolmin <zzmzzm0065@gmail.com>
2024-08-07 02:35:47 +0900

    ✨ Feat: connet mock data with dictation page

---
+++ src/mockData.js
@@ -0,0 +1,35 @@
+// src/mockData.js
+const mockData = [
+    {
+        "id": 4587,
+        "word": "벵에",
+        "meaning": "~병덩이",
+        "soundurl": "https://www.jeju.go.kr/api/culture/dialect?dialect=1560"
+    },
+    {
+        "id": 4943,
+        "word": "듬피",
+        "meaning": "~남포동",
+        "soundurl": "https://www.jeju.go.kr/api/culture/dialect?dialect=1123"
+    },
+    {
+        "id": 1004,
+        "word": "두린굿",
+        "meaning": "정신질환 환자를 치료하는 굿\n",
+        "soundurl": "https://www.jeju.go.kr/api/culture/dialect?dialect=6104"
+    },
+    {
+        "id": 5562,
+        "word": "간세다리",
+        "meaning": "~간세–등이. 게으름장이",
+        "soundurl": "https://www.jeju.go.kr/api/culture/dialect?dialect=374"
+    },
+    {
+        "id": 2436,
+        "word": "마늘",
+        "meaning": "~마농",
+        "soundurl": "https://www.jeju.go.kr/api/culture/dialect?dialect=4322"
+    }
+];
+
+export default mockData;
--- src/pages/DictationPage.jsx
+++ src/pages/DictationPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import styles from '../styles/DictationPage.module.css';
 import background from '../assets/dictationBg.png';
 import note from '../assets/dictationNote.png';
@@ -6,14 +6,18 @@ import { Howl } from 'howler';
 import MidBtn from '../assets/midBtn.png';
 import { HiSpeakerWave } from "react-icons/hi2";
 import { FaQuestionCircle } from "react-icons/fa";
+import { Tooltip } from 'antd';
+import mockData from '../mockData.js';
 
-const questions = [
-    { audioSrc: '/src/assets/sound/흥창망창.mp3', description: '문제 1 설명', correctAnswer: '안녕하수깡 - 안녕하세요' },
-    { audioSrc: '/src/assets/sound/흥창망창.mp3', description: '문제 2 설명', correctAnswer: '안녕하수깡 - 안녕하세요' },
-    { audioSrc: '/src/assets/sound/흥창망창.mp3', description: '문제 3 설명', correctAnswer: '안녕하수깡 - 안녕하세요' },
-    { audioSrc: '/src/assets/sound/흥창망창.mp3', description: '문제 4 설명', correctAnswer: '안녕하수깡 - 안녕하세요' },
-    { audioSrc: '/src/assets/sound/흥창망창.mp3', description: '문제 5 설명', correctAnswer: '안녕하수깡 - 안녕하세요' },
-];
+// mockData를 questions 형식으로 변환
+const questions = mockData.map((item, index) => ({
+    audioSrc: item.soundurl,
+    description: item.meaning,
+    correctAnswer: item.word
+}));
+
+const tooltipText = <p>시간제한이 없으니 잘 듣고 띄어쓰기에 유의하여 정답을 입력해 제출해 보세요.
+    듣기 버튼을 클릭하면 언제든지 다시 들을 수 있습니다.</p>;
 
 const DictationItem = ({ audioSrc, description, correctAnswer, index, showAnswers, resetFlag }) => {
     const [answer, setAnswer] = useState('');
@@ -37,13 +41,12 @@ const DictationItem = ({ audioSrc, description, correctAnswer, index, showAnswer
 
     return (
         <div className={styles.dictationItem}>
-
             <div className={styles.itemTopContainer}>
                 <div className={styles.question}>
                     <span>{index + 1}번 문제: {description}</span>
                 </div>
                 <div className={styles.soundInput}>
-                    <button className={styles.playButton} onClick={playAudio}><HiSpeakerWave/></button>
+                    <button className={styles.playButton} onClick={playAudio}><HiSpeakerWave /></button>
                     <input
                         type="text"
                         value={answer}
@@ -52,10 +55,8 @@ const DictationItem = ({ audioSrc, description, correctAnswer, index, showAnswer
                         className={styles.input}
                     />
                 </div>
-
             </div>
             {showAnswers && <div className={styles.result}>정답: {correctAnswer}</div>}
-
         </div>
     );
 };
@@ -73,11 +74,19 @@ const DictationPage = () => {
         setResetFlag(prevFlag => prevFlag + 1);  // Trigger a reset
     };
 
+    const mergedArrow = useMemo(() => {
+        return {
+            pointAtCenter: true,
+        };
+    }, []);
+
     return (
-        <div className={styles.container} style={{backgroundImage: `url(${background})`}}>
-            <div className={styles.note} style={{backgroundImage: `url(${note})`}}>
+        <div className={styles.container} style={{ backgroundImage: `url(${background})` }}>
+            <div className={styles.note} style={{ backgroundImage: `url(${note})` }}>
                 <div className={styles.questionIcon}>
-                    <FaQuestionCircle/>
+                    <Tooltip placement="bottomRight" title={tooltipText} arrow={mergedArrow}>
+                        <FaQuestionCircle />
+                    </Tooltip>
                 </div>
                 <div className={styles.itemContainer}>
                     {questions.map((question, index) => (
@@ -93,9 +102,9 @@ const DictationPage = () => {
                     ))}
                 </div>
                 <div className={styles.btnConrainer}>
-                    <button className={styles.longBtn} style={{backgroundImage: `url(${MidBtn})`}}
+                    <button className={styles.longBtn} style={{ backgroundImage: `url(${MidBtn})` }}
                             onClick={handleShowAnswers}><span>정답 확인</span></button>
-                    <button className={styles.longBtn} style={{backgroundImage: `url(${MidBtn})`}}
+                    <button className={styles.longBtn} style={{ backgroundImage: `url(${MidBtn})` }}
                             onClick={reload}><span>다시 풀기</span></button>
                 </div>
             </div>
