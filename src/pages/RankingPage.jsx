9f14d911b0c514c30d4d413a89567af057271453
HidenLee <hyundo1995@gmail.com>
2024-08-07 14:45:02 +0900

    Rankingpage logic added

--- src/pages/RankingPage.jsx
+++ src/pages/RankingPage.jsx
@@ -13,27 +13,33 @@ import axios from "axios";
 // You can replace 'PageComponent' with the name of your component
 const RankingPage = () => {
     const [rankings, setRankings] = useState([]);
+    const [isWordRankFlag, setIsWordRankFlag] = useState(true);
+
+
     useEffect(() => {
         // Replace 'your-backend-url' with your actual API endpoint
-        axios.get('https://k5d881cb764f0a.user-app.krampoline.com/api/scores/top10/word')
+        let path = isWordRankFlag ? "word" : "sentence"
+        axios.get(`https://k5d881cb764f0a.user-app.krampoline.com/api/scores/top10/${path}`)
             .then(response => {
                 setRankings(response.data); // Assuming the response data is an array of rankings
             })
             .catch(error => {
                 console.error('Error fetching rankings:', error);
             });
-    }, []);
+    }, [isWordRankFlag]);
     return (
         <div className={styles.container} style={{backgroundImage: `url(${background})`}}>
             <div className={styles.title}>요망진 아이들</div>
             <div className={styles.btnContainer}>
                 <button
                     className={styles.midBtn}
+                    onClick={() => setIsWordRankFlag(true)}
                     style={{backgroundImage: `url(${MidBtn})`}}
                 >단어 순위
                 </button>
                 <button
                     className={styles.midBtn}
+                    onClick={() => setIsWordRankFlag(false)}
                     style={{backgroundImage: `url(${MidBtn})`}}
                 >장문 순위
                 </button>
@@ -46,37 +52,50 @@ const RankingPage = () => {
                             <img src={avatar} alt="avatar-rock" className={styles.avatar}/>
                             <img src={silver} alt="silver-medal" className={styles.medal}/>
                         </div>
-                        <div className={styles.boxNickname}>{rankings[1]? rankings[1].username : "2등쥬디"}</div>
-                        <div className={styles.boxScore}>{rankings[1]? `${rankings[1].score}점` : "30점"}</div>
+                        <div className={styles.boxNickname}>{rankings[1] ? rankings[1].username : "2등쥬디"}</div>
+                        <div className={styles.boxScore}>{rankings[1] ? `${rankings[1].score}점` : "30점"}</div>
                     </div>
                     <div className={styles.boxGroup}>
                         <div className={styles.bigBox}>
                             <img src={avatar} alt="avatar-rock" className={styles.avatar}/>
                             <img src={gold} alt="gold-medal" className={styles.medal}/>
                         </div>
-                        <div className={styles.boxNickname}>{rankings[0]? rankings[0].username : "1등쥬디"}</div>
-                        <div className={styles.boxScore}>{rankings[0]? `${rankings[0].score}점` : "30점"}</div>
+                        <div className={styles.boxNickname}>{rankings[0] ? rankings[0].username : "1등쥬디"}</div>
+                        <div className={styles.boxScore}>{rankings[0] ? `${rankings[0].score}점` : "30점"}</div>
                     </div>
                     <div className={styles.boxGroup}>
                         <div className={styles.smallBox}>
                             <img src={avatar} alt="avatar-rock" className={styles.avatar}/>
                             <img src={bronze} alt="silver-medal" className={styles.medal}/>
                         </div>
-                        <div className={styles.boxNickname}>{rankings[2]? rankings[2].username : "3등쥬디"}</div>
-                        <div className={styles.boxScore}>{rankings[2]? `${rankings[2].score}점` : "30점"}</div>
+                        <div className={styles.boxNickname}>{rankings[2] ? rankings[2].username : "3등쥬디"}</div>
+                        <div className={styles.boxScore}>{rankings[2] ? `${rankings[2].score}점` : "30점"}</div>
                     </div>
                 </div>
                 <div className={styles.rankingContainer}>
-                    <RankingItem index={4} score={rankings[3]? rankings[3].score : 30} username={rankings[3]? rankings[3].username : "4등쥬디"}/>
-                    <RankingItem index={5} score={rankings[4]? rankings[4].score : 30} username={rankings[4]? rankings[4].username : "5등쥬디"}/>
-                    <RankingItem index={6} score={rankings[5]? rankings[5].score : 30} username={rankings[5]? rankings[5].username : "6등쥬디"}/>
-                    <RankingItem index={7} score={rankings[6]? rankings[6].score : 30} username={rankings[6]? rankings[6].username : "7등쥬디"}/>
-                    <RankingItem index={8} score={rankings[7]? rankings[7].score : 30} username={rankings[7]? rankings[7].username : "8등쥬디"}/>
-                    <RankingItem index={9} score={rankings[8]? rankings[8].score : 30} username={rankings[8]? rankings[8].username : "9등쥬디"}/>
-                    <RankingItem index={10} score={rankings[9]? rankings[9].score : 30} username={rankings[9]? rankings[9].username : "10등쥬디"}/>
-
-
+                    {rankings[3] && (
+                        <RankingItem index={4} score={rankings[3].score} username={rankings[3].username}/>
+                    )}
+                    {rankings[4] && (
+                        <RankingItem index={5} score={rankings[4].score} username={rankings[4].username}/>
+                    )}
+                    {rankings[5] && (
+                        <RankingItem index={6} score={rankings[5].score} username={rankings[5].username}/>
+                    )}
+                    {rankings[6] && (
+                        <RankingItem index={7} score={rankings[6].score} username={rankings[6].username}/>
+                    )}
+                    {rankings[7] && (
+                        <RankingItem index={8} score={rankings[7].score} username={rankings[7].username}/>
+                    )}
+                    {rankings[8] && (
+                        <RankingItem index={9} score={rankings[8].score} username={rankings[8].username}/>
+                    )}
+                    {rankings[9] && (
+                        <RankingItem index={10} score={rankings[9].score} username={rankings[9].username}/>
+                    )}
                 </div>
+
             </div>
 
 
